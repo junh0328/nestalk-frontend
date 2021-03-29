5128e3df6e111d44310c026d502b0778c141fd97
이준희 <junh0328@naver.com>
2021-03-29 16:54:05 +0900

    #28 feat: 모달 컴포넌트 생성 및 스타일드 컴포넌트 분리

--- src/components/pages/Email/Email.tsx
+++ src/components/pages/Email/Email.tsx
@@ -1,78 +1,66 @@
-import React from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
-
-const Global = createGlobalStyle`
-    #__next{
-      display: flex;
-      justify-content: center;
-      flex-direction: column;
-      align-items: center;
-    }
-`;
-
-const CardWrapper = styled.div`
-  width: 400px;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  border: 1px solid #e5e5e5;
-  padding: 20px;
-
-  .title {
-    margin-top: 20px;
-    font-size: 1.4rem;
-  }
-`;
-
-const Card = styled.div`
-  width: 300px;
-  height: 90px;
-  border-bottom: 2px solid #e5e5e5;
-  margin-bottom: 10px;
-  cursor: pointer;
-
-  .top {
-    margin-top: 10px;
-    color: black;
-  }
-
-  :first-child {
-    border-bottom: none;
-    height: 60px;
-    cursor: auto;
-  }
-
-  :last-child {
-    margin-top: 20px;
-    border-bottom: none;
-    height: 60px;
-    font-size: 0.6rem;
-    cursor: auto;
-  }
-
-  p {
-    color: gray;
-  }
-`;
+import Modal from '@src/components/frames/Modal/Modal';
+import React, { useCallback, useState } from 'react';
+import { CardWrapper, Card, Global, Input } from './styled';
 
 const Email = () => {
+  const [showPhoneModal, setShowPhoneModal] = useState(false);
+  const [showEmailModal, setShowEmailModal] = useState(false);
+
+  const onClickPhoneModal = useCallback((prev) => {
+    setShowPhoneModal((prev) => !prev);
+  }, []);
+
+  const onClickEmailModal = useCallback((prev) => {
+    setShowEmailModal((prev) => !prev);
+  }, []);
+
+  const onCloseModal = useCallback(() => {
+    setShowEmailModal(false);
+    setShowPhoneModal(false);
+  }, []);
+
   return (
     <>
       <Global />
       <div style={{ textAlign: 'center', marginBottom: 30, fontSize: '2rem' }}> Kakao</div>
       <CardWrapper>
         <Card className="title">카카오 계정을 찾을 방법을 선택해주세요</Card>
-        <Card onClick={() => alert('본인 인증으로 이동')}>
+        <Card onClick={onClickPhoneModal}>
           <p className="top">본인 인증</p>
           <p style={{ marginTop: 20 }}>휴대폰에 등록된 본인 확인 정보로 찾습니다.</p>
         </Card>
-        <Card onClick={() => alert('이메일 찾기로 이동')}>
+        <Card onClick={onClickEmailModal}>
           <p className="top">이메일로 찾기</p>
           <p style={{ marginTop: 20 }}>카카오 서비스에서 이용한 프로필 및 계정의 연락처 이메일로 계정을 찾습니다.</p>
         </Card>
         <Card className="footer">비즈니스 계정을 찾으시나요?</Card>
       </CardWrapper>
+      {showPhoneModal && (
+        <Modal show={showPhoneModal} onCloseModal={onCloseModal}>
+          <form>
+            <span>가입하신 휴대폰 번호를 입력해주세요</span>
+            <div>
+              <Input />
+            </div>
+            <button type="submit" style={{ marginTop: 200, width: 100, height: 30 }}>
+              본인 인증하기
+            </button>
+          </form>
+        </Modal>
+      )}
+      {showEmailModal && (
+        <Modal show={showEmailModal} onCloseModal={onCloseModal}>
+          <form>
+            <span>가입하신 이메일 정확히 입력해주세요</span>
+            <div>
+              <Input />
+            </div>
+            <button type="submit" style={{ marginTop: 200, width: 100, height: 30 }}>
+              본인 인증하기
+            </button>
+          </form>
+        </Modal>
+      )}
     </>
   );
 };
---
+++ src/components/pages/Email/styled.ts
@@ -0,0 +1,62 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const Global = createGlobalStyle`
+    #__next{
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+      align-items: center;
+    }
+`;
+
+export const CardWrapper = styled.div`
+  width: 400px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid #e5e5e5;
+  padding: 20px;
+
+  & .title {
+    margin-top: 20px;
+    font-size: 1.4rem;
+  }
+`;
+
+export const Card = styled.div`
+  width: 300px;
+  height: 90px;
+  border-bottom: 2px solid #e5e5e5;
+  margin-bottom: 10px;
+  cursor: pointer;
+
+  & .top {
+    margin-top: 10px;
+    color: black;
+  }
+
+  & :first-child {
+    border-bottom: none;
+    height: 60px;
+    cursor: auto;
+  }
+
+  & :last-child {
+    margin-top: 20px;
+    border-bottom: none;
+    height: 60px;
+    font-size: 0.6rem;
+    cursor: auto;
+  }
+
+  & p {
+    color: gray;
+  }
+`;
+
+export const Input = styled.input`
+  width: 250px;
+  padding: 5px;
+  margin: 20px 0px;
+`;
